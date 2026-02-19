a41f546d99cd9c9a6e1b2ea1730614fa191f38b8
Camilo-Suar3z <brayan_cami@hotmail.com>
2026-02-19 13:55:48 -0500

    feat: mejora manejo de errores para enums inválidos corregida

--- src/main/java/com/brayansuarez/todo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/brayansuarez/todo/exception/GlobalExceptionHandler.java
@@ -1,60 +1,60 @@
-package com.brayansuarez.todo.exception;
- import org.aspectj.bridge.IMessage;
- import org.springframework.http.HttpStatus;
- import org.springframework.http.ResponseEntity;
- import org.springframework.http.converter.HttpMessageNotReadableException;
- import org.springframework.web.HttpMediaTypeException;
- import org.springframework.web.bind.annotation.ControllerAdvice;
- import org.springframework.web.bind.annotation.ExceptionHandler;
+    package com.brayansuarez.todo.exception;
+     import org.aspectj.bridge.IMessage;
+     import org.springframework.http.HttpStatus;
+     import org.springframework.http.ResponseEntity;
+     import org.springframework.http.converter.HttpMessageNotReadableException;
+     import org.springframework.web.HttpMediaTypeException;
+     import org.springframework.web.bind.annotation.ControllerAdvice;
+     import org.springframework.web.bind.annotation.ExceptionHandler;
 
- import java.rmi.MarshalledObject;
- import java.time.LocalDate;
- import java.time.LocalDateTime;
- import java.util.HashMap;
- import java.util.Map;
+     import java.rmi.MarshalledObject;
+     import java.time.LocalDate;
+     import java.time.LocalDateTime;
+     import java.util.HashMap;
+     import java.util.Map;
 
- @ControllerAdvice
+     @ControllerAdvice
 
-public class GlobalExceptionHandler {
+    public class GlobalExceptionHandler {
 
-@ExceptionHandler(TaskValidationException.class)
-     public ResponseEntity<Map<String,Object>> handleTaskValidationExeption(TaskValidationException ex) {
-    Map<String, Object> response = new HashMap<>();
-    response.put("timestamp", LocalDateTime.now());
-    response.put("status", HttpStatus.BAD_REQUEST.value());
-    response.put("error", "validation error");
-    response.put("message", ex.getMessage());
-    response.put("path", "api/tasks");
-    return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(TaskValidationException.class)
+         public ResponseEntity<Map<String,Object>> handleTaskValidationExeption(TaskValidationException ex) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("timestamp", LocalDateTime.now());
+        response.put("status", HttpStatus.BAD_REQUEST.value());
+        response.put("error", "validation error");
+        response.put("message", ex.getMessage());
+        response.put("path", "api/tasks");
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
 
 
-}
+    }
 
-     @ExceptionHandler(HttpMediaTypeException.class)
-     public ResponseEntity<Map<String, Object>> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
-         Map<String, Object> response = new HashMap<>();
-         response.put("timestamp", LocalDateTime.now());
-         response.put("Status", HttpStatus.BAD_REQUEST.value());
-         response.put("erro", "validator error");
+         @ExceptionHandler(HttpMessageNotReadableException.class)
+         public ResponseEntity<Map<String, Object>> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+             Map<String, Object> response = new HashMap<>();
+             response.put("timestamp", LocalDateTime.now());
+             response.put("Status", HttpStatus.BAD_REQUEST.value());
+             response.put("erro", "validator error");
 
-         String message = ex.getMessage();
-         String customMessage = "Formato de JSON inválido";
-         if (message.contains("TaskStatus")) {
-             customMessage = "El estado debe ser uno de: PENDING, IN_PROGRESS, DONE";
-         } else if (message.contains("TaskPriority")) {
-             customMessage = "La prioridad debe ser uno de: LOW, MEDIUM, HIGH";
-         } else if (message.contains("LocalDate")) {
+             String message = ex.getMessage();
+             String customMessage = "Formato de JSON inválido";
+             if (message.contains("TaskStatus")) {
+                 customMessage = "El estado debe ser uno de: PENDING, IN_PROGRESS, DONE";
+             } else if (message.contains("TaskPriority")) {
+                 customMessage = "La prioridad debe ser uno de: LOW, MEDIUM, HIGH";
+             } else if (message.contains("LocalDate")) {
 
-             customMessage = "La fecha debe tener formato YYYY-MM-DD (ej: 2026-12-31)";
+                 customMessage = "La fecha debe tener formato YYYY-MM-DD (ej: 2026-12-31)";
+             }
+
+
+             response.put("message", customMessage);
+             response.put("path", "api/tasks");
+             return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+
          }
 
 
-         response.put("message", customMessage);
-         response.put("path", "api/tasks");
-         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-
-     }
-
-
-}
+    }
 
