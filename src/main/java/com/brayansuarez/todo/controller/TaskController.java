d392a8615104636312a55c299092d51efbe2fbf0
Camilo-Suar3z <brayan_cami@hotmail.com>
2026-02-19 17:47:57 -0500

    fix: agrega URL de frontend en Vercel a CORS

--- src/main/java/com/brayansuarez/todo/controller/TaskController.java
+++ src/main/java/com/brayansuarez/todo/controller/TaskController.java
@@ -22,7 +22,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:3000" )
+@CrossOrigin(origins ={ "http://localhost:3000",
+        "https://todo-app-front-v2.vercel.app" })
+
 @RestController
 @RequestMapping("/api/v1/tasks")
 public class TaskController {
