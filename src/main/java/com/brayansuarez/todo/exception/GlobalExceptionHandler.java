fce6c7ffc8dc3a2f213fb2ca3cc9f6a8dafd5abf
Camilo-Suar3z <brayan_cami@hotmail.com>
2026-02-19 13:46:47 -0500

    feat: mejora manejo de errores para enums inválidos

--- src/main/java/com/brayansuarez/todo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/brayansuarez/todo/exception/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.brayansuarez.todo.exception;
  import org.aspectj.bridge.IMessage;
  import org.springframework.http.HttpStatus;
  import org.springframework.http.ResponseEntity;
+ import org.springframework.http.converter.HttpMessageNotReadableException;
+ import org.springframework.web.HttpMediaTypeException;
  import org.springframework.web.bind.annotation.ControllerAdvice;
  import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -28,6 +30,30 @@ public class GlobalExceptionHandler {
 
 }
 
+     @ExceptionHandler(HttpMediaTypeException.class)
+     public ResponseEntity<Map<String, Object>> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+         Map<String, Object> response = new HashMap<>();
+         response.put("timestamp", LocalDateTime.now());
+         response.put("Status", HttpStatus.BAD_REQUEST.value());
+         response.put("erro", "validator error");
+
+         String message = ex.getMessage();
+         String customMessage = "Formato de JSON inválido";
+         if (message.contains("TaskStatus")) {
+             customMessage = "El estado debe ser uno de: PENDING, IN_PROGRESS, DONE";
+         } else if (message.contains("TaskPriority")) {
+             customMessage = "La prioridad debe ser uno de: LOW, MEDIUM, HIGH";
+         } else if (message.contains("LocalDate")) {
+
+             customMessage = "La fecha debe tener formato YYYY-MM-DD (ej: 2026-12-31)";
+         }
+
+
+         response.put("message", customMessage);
+         response.put("path", "api/tasks");
+         return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+
+     }
 
 
 }
